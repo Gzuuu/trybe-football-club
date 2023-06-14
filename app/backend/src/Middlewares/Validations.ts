daf3ffc3365b0c022165dae15e06304c49e6a029
Gzuuu <jesue_gege@hotmail.com>
2023-06-14 19:25:59 -0300

    cria validations para login

---
+++ app/backend/src/Middlewares/Validations.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from 'express';
+
+class Validations {
+    private requiredKeys: string[];
+    constructor(keys: string[]){
+        this.requiredKeys = keys;
+    }
+   validateUser(req: Request, res: Response, next: NextFunction): Response | void {
+    const book = req.body;
+    const notFoundKey = this.requiredKeys.find((key) => !(key in book));
+    if (notFoundKey) {
+      return res.status(400).json({ message: `All fields must be filled` });
+    }
+
+    next();
+  }
+
+  
+}
+
+export default Validations;
--- app/backend/src/Routes/user.routes.ts
+++ app/backend/src/Routes/user.routes.ts
@@ -1,10 +1,15 @@
-import { Request, Router, Response } from 'express';
+import { Request, Router, Response, NextFunction } from 'express';
 import UserController from '../Controller/UserController';
+import Validations from '../Middlewares/Validations';
 
 const userController = new UserController();
+const loginBody = ['email', 'password'];
+const validate = new Validations(loginBody);
 
 const router = Router();
 
-router.post('/', (req: Request, res: Response) => userController.login(req, res));
+router.post('/',
+(req: Request, res: Response, next: NextFunction) => validate.validateUser(req, res, next),
+(req: Request, res: Response) => userController.login(req, res));
 
 export default router;
