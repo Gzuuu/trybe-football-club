8ae4a5e0ab94ed09b52025ab9798228fdfce8d52
Gzuuu <jesue_gege@hotmail.com>
2023-06-14 19:26:18 -0300

    corrige linter

--- app/backend/src/Middlewares/Validations.ts
+++ app/backend/src/Middlewares/Validations.ts
@@ -1,21 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
 
 class Validations {
-    private requiredKeys: string[];
-    constructor(keys: string[]){
-        this.requiredKeys = keys;
-    }
-   validateUser(req: Request, res: Response, next: NextFunction): Response | void {
+  private requiredKeys: string[];
+  constructor(keys: string[]) {
+    this.requiredKeys = keys;
+  }
+
+  validateUser(req: Request, res: Response, next: NextFunction): Response | void {
     const book = req.body;
     const notFoundKey = this.requiredKeys.find((key) => !(key in book));
     if (notFoundKey) {
-      return res.status(400).json({ message: `All fields must be filled` });
+      return res.status(400).json({ message: 'All fields must be filled' });
     }
 
     next();
   }
-
-  
 }
 
 export default Validations;
--- app/backend/src/Routes/user.routes.ts
+++ app/backend/src/Routes/user.routes.ts
@@ -8,8 +8,10 @@ const validate = new Validations(loginBody);
 
 const router = Router();
 
-router.post('/',
-(req: Request, res: Response, next: NextFunction) => validate.validateUser(req, res, next),
-(req: Request, res: Response) => userController.login(req, res));
+router.post(
+  '/',
+  (req: Request, res: Response, next: NextFunction) => validate.validateUser(req, res, next),
+  (req: Request, res: Response) => userController.login(req, res),
+);
 
 export default router;
