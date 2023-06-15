0a49952025b9432646d80f9c8a8b3130bcf0f6db
Gzuuu <jesue_gege@hotmail.com>
2023-06-15 18:11:00 -0300

    cria match service

---
+++ app/backend/src/Services/MatchService.ts
@@ -0,0 +1,15 @@
+import { ServiceResponse } from "../Interfaces/ServiceResponse";
+import { IMatchModelType, IMatches } from "../Interfaces/MatchesMigrate";
+import MatchModel from "../models/MatchModel";
+
+export default class MatchService {
+    constructor(
+        private model: IMatchModelType = new MatchModel()
+    ) {}
+
+    public async getAllMatches(): Promise<ServiceResponse<IMatches[]>> {
+        const allMatches = await this.model.findAll();
+
+        return { status: 'SUCCESSFUL', data: allMatches };
+    }
+}
