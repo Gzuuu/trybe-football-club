b2c327015fe82324c53151502288580c9db094a6
Gzuuu <jesue_gege@hotmail.com>
2023-06-19 17:09:12 -0300

    req 30 concluido

--- app/backend/src/Routes/leaderboard.routes.ts
+++ app/backend/src/Routes/leaderboard.routes.ts
@@ -6,8 +6,18 @@ const leaderboardController = new LeaderBoardController();
 const router = Router();
 
 router.get(
+  '/',
+  (req: Request, res: Response) => leaderboardController.getLeaderboardInfo(req, res),
+);
+
+router.get(
   '/home',
   (req: Request, res: Response) => leaderboardController.getHomeLeadeboards(req, res),
+);
+
+router.get(
+  '/away',
+  (req: Request, res: Response) => leaderboardController.getAwayLeaderboards(req, res),
 );
 
 export default router;
